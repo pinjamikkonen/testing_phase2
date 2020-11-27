3ecd456e1b453305ce1ec2bc7ba033c75a0abf58
Pinja <pinja.mikkonen@tuni.fi>
2020-11-27 13:20:41 +0200

    errorviestit joista lähetetty opelle viestiä

--- test/test1.js
+++ test/test1.js
@@ -1,8 +1,18 @@
 var assert = require('assert');
+var clamp = require('../src/clamp');
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return 10 idk bruh', function() {
+      assert.equal(clamp(10, -5, 5), 5);
     });
   });
 });
