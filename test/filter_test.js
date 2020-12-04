55d9b522b7f443e4fbd5b641b3ba0b798996fb76
Timo Tuulio <timotuulio@hotmail.com>
2020-12-04 12:04:12 +0200

    Add tests for filter

--- test/filter_test.js
+++ test/filter_test.js
@@ -12,7 +12,30 @@ const users = [
 describe('filter', function() {
   describe('Filter non-active out', function() {
     it('Should return Diego and Bananaman', function() {
-      assert.equal(filter(users, ({ active }) => active), 5);
+      assert.deepEqual(filter(users, ({ active }) => active), [
+        { 'user': 'Diego', 'active': true },
+        { 'user': 'Bananaman', 'active': true }
+      ]);
+    });
+  });
+
+  describe('Filter with non-boolean attribute', function() {
+    it('Should return Diego', function() {
+      assert.deepEqual(filter(users, ({ user }) => "Diego"), [
+        { 'user': 'Diego', 'active': true }
+      ]);
+    });
+  });
+
+  describe('Filter with attribute that doesnt exist', function() {
+    it('Should return empty list', function() {
+      assert.deepEqual(filter(users, ({ powerlevel }) => 9001), [[]]);
+    });
+  });
+
+  describe('Filter a null-value', function() {
+    it('Should return empty list', function() {
+      assert.deepEqual(filter(null, ({ active }) => active), [[]]);
     });
   });
 });
