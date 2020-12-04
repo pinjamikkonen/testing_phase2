2fe2f15dd97c6fd2d342105c225237d387327cb7
Timo Tuulio <timotuulio@hotmail.com>
2020-12-04 14:05:23 +0200

    Add castArray test

--- test/castArray_test.js
+++ test/castArray_test.js
@@ -1,13 +1,31 @@
 import pkg from 'chai'
-import at from '../src/at.js'
+import castArray from '../src/castArray.js'
 var { assert } = pkg;
 
-const object = { 'a': [{ 'b': { 'c': 3 } }, 4] }
+var arr = [['a', 'b']]
 
-describe('at', function() {
-  describe('Test with nonexistent value', function() {
-    it('Should recover', function() {
-      assert.deepEqual(at(object, ['e']), [undefined]);
+describe('castArray', function() {
+  describe('Test with an int', function() {
+    it('Should return parameters as an array', function() {
+      assert.deepEqual(castArray(1), [1]);
+    });
+  });
+
+  describe('Test with no parameters', function() {
+    it('Should return an enpty array', function() {
+      assert.deepEqual(castArray(), []);
+    });
+  });
+
+  describe('Test with array of arrays', function() {
+    it('Should return the same value as parameter', function() {
+      assert.deepEqual(castArray(arr), [['a', 'b']]);
+    });
+  });
+
+  describe('Test with multiple parameters', function() {
+    it('Should restore', function() {
+      assert.isOk(castArray('a', 12, true));
     });
   });
 });
