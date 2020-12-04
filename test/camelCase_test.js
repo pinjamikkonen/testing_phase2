184a966f05f60fb1916b49633991d2d6a6992976
Timo Tuulio <timotuulio@hotmail.com>
2020-12-04 13:21:08 +0200

    Add camelCase tests

---
+++ test/camelCase_test.js
@@ -0,0 +1,43 @@
+import pkg from 'chai'
+import camelCase from '../src/camelCase.js'
+var { assert, expect, should } = pkg;
+
+var arr = [true, false, false, true];
+
+describe('camelCase', function() {
+  describe('Change string to camelcase', function() {
+    it('Should return zergIsOverPowered', function() {
+      assert.equal(camelCase("   zerG iS OVer PoWreD"), "zergIsOverPowered");
+    });
+  });
+
+  describe('Change a simple string to camelcase', function() {
+    it('Should return aapeli', function() {
+      assert.equal(camelCase("   aapeli"), "aapeli");
+    });
+  });
+
+  describe('Change string with hyphen to camelcase', function() {
+    it('Should return thisWorks', function() {
+      assert.equal(camelCase("tHIS-- -- worKs"), "thisWorks");
+    });
+  });
+
+  describe('Try to change a double to camelcase', function() {
+    it('Should return 15982', function() {
+      assert.equal(camelCase(15.982), 15982);
+    });
+  });
+
+  describe('Try to change an array to camelcase', function() {
+      it('Should return trueFalseFalseTrue', function() {
+        assert.equal(camelCase(arr), "trueFalseFalseTrue");
+      });
+  });
+
+  describe('Give undefined parameter to function', function() {
+      it('Should return empty or undefined', function() {
+        assert.deepEqual(camelCase(undefined), undefined);
+      });
+  });
+});
