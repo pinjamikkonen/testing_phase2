78100ccc04d009f87b10ff9bfab6ee99f68a6b47
Pinja <pinja.mikkonen@tuni.fi>
2020-12-05 17:05:28 +0200

    isObjectLike testattu

--- test/isObjectLike_test.js
+++ test/isObjectLike_test.js
@@ -1,73 +1,72 @@
 import pkg from 'chai'
-import isObject from '../src/isObject.js'
+import isObjectLike from '../src/isObjectLike.js'
 var { assert, expect, should } = pkg;
 
-describe('isObject', function() {
+describe('isObjectLike', function() {
 
   describe('x.1. {}', function() {
     it('Should return true', function() {
-      assert.equal(isObject({}), true);
+      assert.equal(isObjectLike({}), true);
     });
   });
 
   describe('x.2. [1, 2, 3]', function() {
     it('Should return true', function() {
-      assert.equal(isObject([1, 2, 3]), true);
+      assert.equal(isObjectLike([1, 2, 3]), true);
     });
   });
 
   describe('x.3. Function', function() {
-    it('Should return true', function() {
-      assert.equal(isObject(Function), true);
+    it('Should return false', function() {
+      assert.equal(isObjectLike(Function), false);
     });
   });
 
   describe('x.4. new Number(55)', function() {
     it('Should return true', function() {
-      assert.equal(isObject(new Number(55)), true);
+      assert.equal(isObjectLike(new Number(55)), true);
     });
   });
 
   describe('x.5. new String(test_string)', function() {
     it('Should return true', function() {
-      assert.equal(isObject(new String('test_string')), true);
+      assert.equal(isObjectLike(new String('test_string')), true);
     });
   });
 
   describe('x.6. new RegExp(\\w+)', function() {
     it('Should return true', function() {
-      assert.equal(isObject(new RegExp('\\w+')), true);
+      assert.equal(isObjectLike(new RegExp('\\w+')), true);
     });
   });
 
   describe('x.7. null', function() {
     it('Should return false', function() {
-      assert.equal(isObject(null), false);
+      assert.equal(isObjectLike(null), false);
     });
   });
 
   describe('x.8. NaN', function() {
     it('Should return false', function() {
-      assert.equal(isObject(NaN), false);
+      assert.equal(isObjectLike(NaN), false);
     });
   });
 
   describe('x.9. test_string', function() {
     it('Should return false', function() {
-      assert.equal(isObject('test_string'), false);
+      assert.equal(isObjectLike('test_string'), false);
     });
   });
 
   describe('x.10. true', function() {
     it('Should return false', function() {
-      assert.equal(isObject(true), false);
+      assert.equal(isObjectLike(true), false);
     });
   });
 
   describe('x.11. 55', function() {
     it('Should return false', function() {
-      assert.equal(isObject(55), false);
+      assert.equal(isObjectLike(55), false);
     });
   });
-
 });
