93ee763414f39bf347263bde70f36acf5427fe9b
Pinja <pinja.mikkonen@tuni.fi>
2020-12-06 22:55:40 +0200

    lisää tulosteita siistitty v8

--- test/words_test.js
+++ test/words_test.js
@@ -2,69 +2,69 @@ import pkg from 'chai'
 import words from '../src/words.js'
 var { assert, expect, should } = pkg;
 
-describe('words', function() {
+describe('35. words', function() {
 
-  describe('x.1. titi kissa on ihana', function() {
-    it('Should return [titi, kissa, on, ihana]', function() {
+  describe('35.1. Input is a string ("titi kissa on ihana")', function() {
+    it('Should return ["titi", "kissa", "on", "ihana"]', function() {
       assert.deepEqual(words('titi kissa on ihana'), ['titi', 'kissa', 'on', 'ihana']);
     });
   });
 
-  describe('x.2. titi titi titi kissa', function() {
-    it('Should return [titi, titi, titi, kissa]', function() {
+  describe('35.2. Input is a repeating string ("titi titi titi kissa")', function() {
+    it('Should return ["titi", "titi", "titi", "kissa"]', function() {
       assert.deepEqual(words('titi titi titi kissa'), ['titi', 'titi', 'titi', 'kissa']);
     });
   });
 
-  describe('x.3. titi = kissa', function() {
-    it('Should return [titi, kissa]', function() {
+  describe('35.3. Input is a string with a symbol ("titi = kissa")', function() {
+    it('Should return ["titi", "kissa"]', function() {
       assert.deepEqual(words('titi = kissa'), ['titi', 'kissa']);
     });
   });
 
-  describe('x.4. titi = kissa, symbols', function() {
-    it('Should return [titi, =, kissa]', function() {
+  describe('35.4. Input is a string and regular expression ("titi = kissa", /[^, ]+/g)', function() {
+    it('Should return ["titi", "=", "kissa"]', function() {
       assert.deepEqual(words('titi = kissa',  /[^, ]+/g), ['titi', '=', 'kissa']);
     });
   });
 
-  describe('x.5. = 0 === 11 44 &&&, symbols', function() {
+  describe('35.5. Input is a string of numbers and special characters and regular expression ("= 0 === 11 44 &&&", /[^, ]+/g)', function() {
     it('Should return [= 0 === 11 44 &&&]', function() {
       assert.deepEqual(words('= 0 === 11 44 &&&',  /[^, ]+/g), ['=', '0', '===', '11', '44', '&&&']);
     });
   });
 
-  describe('x.6. = 0 === 11 44 &&&, no symbols', function() {
-    it('Should return [0, 11, 44]', function() {
+  describe('35.6. Input is a string of special characters without regular expression ("= 0 === 11 44 &&&"', function() {
+    it('Should return ["0", "11", "44"]', function() {
       assert.deepEqual(words('= 0 === 11 44 &&&'), ['0', '11', '44']);
     });
   });
 
-  // describe('x.7. 44 + 44, numbers', function() {
-  //   it('Should recover from error', function() {
-  //     assert.deepEqual(words(44+44), []);
-  //   });
-  // });
+  describe('35.7. Input is a calculation (44 + 44)', function() {
+    it('Should return an empty array', function() {
+      assert.deepEqual(words(44 + 44), []);
+    });
+  });
 
-  // describe('x.8. null', function() {
-  //   it('Should recover from error', function() {
-  //     assert.deepEqual(words(null), []);
-  //   });
-  // });
+  describe('35.8. Input is null', function() {
+    it('Should return an empty array', function() {
+      assert.deepEqual(words(null), []);
+    });
+  });
 
-  describe('x.9. x.4 titi = kissa, null pattern', function() {
-    it('Should return []', function() {
+  describe('35.9. Input is a string, regexp is null ("titi = kissa", null)', function() {
+    it('Should return an empty array', function() {
       assert.deepEqual(words('titi = kissa',  null), []);
     });
   });
 
-  describe('x.10. x.4 titi = kissa, NaN pattern', function() {
-    it('Should return []', function() {
+  describe('35.10. Input is a string, regexp is NaN ("titi = kissa", NaN)', function() {
+    it('Should return an empty array', function() {
       assert.deepEqual(words('titi = kissa',  NaN), []);
     });
   });
 
-  describe('x.11. Empty input', function() {
+  describe('35.11. Empty input', function() {
     it('Should recover from error', function() {
       assert.deepEqual(words(), []);
     });
